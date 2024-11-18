cd29575a1ceed2bec0e465324c5a5e04e97315cb
CurtFoil <zihel@nycstudents.net>
2024-11-17 20:24:54 -0500

    Practice Problem 1 Completed

--- PracticeProblems.java
+++ PracticeProblems.java
@@ -1,3 +1,11 @@
 public class PracticeProblems {
-    
+    static int countOccurrences(int[] arr, int n) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) { 
+            if (arr[i] == n) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
