06a86d3824265220cb70bce127d1e8b2067d4f67
CurtFoil <zihel@nycstudents.net>
2024-11-20 17:44:27 -0500

    reorganization

--- Main.java
+++ Main.java
@@ -1,32 +1,11 @@
-// public class Main {
-//     static int x = 6;
-//     static double[] arr = {4.5, 6, 9.0, 8};
-//     static boolean[] arr = new boolean[100];
-// 	public static void main(String[] args) {
-//         System.out.println("Hello world");
-// 		System.out.println("x: " + x);
-// 		x = x + 4;
-// 		System.out.println("new x: " + x);
-//         System.out.println(arr[0]);
-//         arr[3] = true;
-//         System.out.println(arr[0] || arr[3]);
-//         System.out.println(arr.length);
-// 	}
-// }
-// public class Main {
-//     static int[][] board = new int[3][3];
-//     static double f(double x) {
-//         return 2 * x;
-//     }
-// 	public static void main(String[] args) {
-//         int x = 3;
-//         board[0][0] = 5;
-//         int firstVal = board[0][0];
-//         System.out.println(firstVal);
-//         System.out.println("f(3): " + f(x));
-// 	}
-// }
-public class Main{
+public class Main {
+    // static int x = 6;
+    // static double[] arr = {4.5, 6, 9.0, 8};
+    // static boolean[] arr = new boolean[100];
+    // static int[][] board = new int[3][3];
+    // static double f(double x) {
+    //     return 2 * x;
+    // }
     // static void printIntArray(int[] arr) {
     //     String str = "(";
     //     for (int i = 0; i < arr.length; i++) {
@@ -54,7 +33,24 @@ public class Main{
         return str + ")";
     }
     public static void main(String[] args){
-        int[] arr = {1, 4, 6, 9};
-	    System.out.println(intArrToString(arr));
+        // System.out.println("Hello world");
+		// System.out.println("x: " + x);
+		// x = x + 4;
+		// System.out.println("new x: " + x);
+        // System.out.println(arr[0]);
+        // arr[3] = true;
+        // System.out.println(arr[0] || arr[3]);
+        // System.out.println(arr.length);
+
+        // int x = 3;
+        // board[0][0] = 5;
+        // int firstVal = board[0][0];
+        // System.out.println(firstVal);
+        // System.out.println("f(3): " + f(x));
+
+        // int[] arr = {1, 4, 6, 9};
+	    // System.out.println(intArrToString(arr));
+
+
     }
 }
